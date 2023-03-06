2f80b457226a9fdc3a95e9865c194d0327b0e36e
Amália Coelho <86680741+amalia-coelho@users.noreply.github.com>
2023-03-06 12:25:39 -0300

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,7 +1,25 @@
 <?php
-    if (!isset($_SESSION['cd'])) {
+    session_start();
+    if (!isset($_SESSION['email'])) {
         header('Location: index.php');
     }else{
         echo 'BEM VINDO FILHO DA PUTA!';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    LOGADO 
+    <?php 
+        echo "Bem vindo".$_POST['nome'];
+    ?>
+</body>
+</html>
+<?php
     }
 ?>
