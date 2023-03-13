931a57ef1ab916400c97b1bb2370c697e8135dcc
Amália Coelho <gray.coelho@gmail.com>
2023-03-13 20:57:53 -0300

    tentativa n2 - script login

--- php/script_login.php
+++ php/script_login.php
@@ -3,19 +3,18 @@
         session_start();
         include('conexao.php');
 
-        $stmt = $conn->prepare("SELECT * FROM tb_usuario WHERE ds_login = :email");
+        $stmt = $conn->prepare("SELECT * FROM tb_usuario WHERE ds_login = :email AND ds_senha =  :senha");
         $stmt->bindValue(':email', $_POST['email']);
+        $stmt->bindValue(':senha', $_POST['senha']);
         $stmt->execute();
         
         $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
         // SE O EMAIL JÁ FOR CADASTRADO
-        if($usuario){
-
-            echo "Usuário encontrado!";
-            
+        if($usuario['ds_login'] == $_POST['email'] && $usuario['ds_senha'] == $_POST['senha']){
+            echo "Pronto para fazer login!";
         }else{
-            echo "Usuário não cadastrado!";
+            echo "Email ou senha incorretos";
         }
     }catch(PDOException $e){
         echo 'ERROR: ' . $e->getMessage();
