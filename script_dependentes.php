f2dc6b87fe4c6b15e39fad5b8b4935e06589a6d2
Amália Coelho <86680741+amalia-coelho@users.noreply.github.com>
2023-05-04 09:34:57 -0300

    add dependente - funcionando

---
+++ script_dependentes.php
@@ -0,0 +1,28 @@
+<?php
+    try{
+        session_start();
+        include('conexao.php');
+        
+        $stmt = $conn->prepare("SELECT * FROM tb_usuario WHERE ds_login = :email");
+        $stmt->bindValue(':email', $_POST['email']);
+        $stmt->execute();
+        
+        $dependente = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        // SE O EMAIL JA FOR CADASTRADO
+        if ($dependente){
+            $stmt = $conn->prepare("UPDATE tb_usuario SET id_responsavel = :id_responsavel, id_nivel = 2 WHERE cd_usuario = :cd_usuario");
+            $stmt->execute(array(
+                ':id_responsavel' => $_SESSION['cd'],
+                ':cd_usuario' => $dependente['cd_usuario']
+            ));
+
+            echo "<p>Usuário cadastrado como dependente!</p>";
+        }else{
+            echo "<p>Email incorreto ou usuário não cadastrado!</p><meta http-equiv='refresh' content='1'>";
+        }
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+        echo "<br>".$stmt->rowCount();
+    }
+?>
