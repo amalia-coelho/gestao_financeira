9c2d1d9e763c8cecc58c4af6607bd98d293a594c
Eric Junokas <95892758+Ericofff@users.noreply.github.com>
2023-05-06 14:36:05 -0300

    home

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if (isset($_SESSION['email'])){
+    if (!isset($_SESSION['email'])){
         header('Location: index.php');
     }else{
 ?>
@@ -14,15 +14,55 @@
     <!----======== CSS ======== -->
     <link rel="stylesheet" href="css/homepag.css">
     <!-- BOOSTRAP -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 
     <!----===== BootStrap 5 CSS ===== -->
-
-         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 
     <!----===== Iconscout CSS ===== -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <!-- JQUERY AJAX -->
+	<script type="text/javascript">
+		$(document).ready(function(){
+            $("#concluir").click(function(){
+
+                // declaração de variáveis
+                var descricao = $('#descricao').val();
+                var data = $('#data').val();
+                var valor = $('#valor').val();
+                var categoria = $('#categoria').val();
+                var pagamento = $('#pagamento').val();
+                var responsavel = $('#responsavel').val();
+
+                $.ajax({
+                url: "php/script_lancamento.php",
+                type: "POST",
+                data: "descricao="+descricao+"&data="+data+"&valor="+valor+"&categoria="+categoria+"&pagamento="+pagamento+"&responsavel="+responsavel,
+                dataType: "html"
+
+                }).done(function(resposta){
+                    // exibir os lançamentos
+                    $('#exibe').html(resposta);
+
+                    // Limpar os inputs
+                    $('#responsavel').val(' ');
+                    $('#pagamento').val(' ');
+                    $('#categoria').val(' ');
+                    $('#valor').val(' ');
+                    $('#data').val(' ');
+                    $('#descricao').val(' ');
+
+                    //fechar o modal
+                    $('#fechar').click();
+                }).fail(function(jqXHR, textStatus ) {
+                    console.log("Request failed: " + textStatus);
+                });
+            });
+		});
+	</script>
+    
 </head>
 <body>
     <nav>
@@ -52,12 +92,18 @@
                     <i class="uil uil-thumbs-up"></i>
                     <span class="link-name">Responsável</span>
                 </a></li>
-                <li><a href="dependentes.php">
-                    <i class="uil uil-comments"></i>
-                    <span class="link-name">Dependente</span>
-                </a></li>
+                <?php 
+                    if($_SESSION['id_nivel'] == 1){
+                    ?>  
+                    <li><a href="dependentes.php">
+                        <i class="uil uil-comments"></i>
+                        <span class="link-name">Dependente</span>
+                    </a></li>
+                    <?php
+                    }
+                ?>
             </ul>
-            <ul class="logout-mode">                <li><a href="#">
+        <ul class="logout-mode"><li><a href="logout.php">
                 <i class="uil uil-signout"></i>
                 <span class="link-name">Logout</span>
             </a></li>
@@ -84,7 +130,7 @@
                 <input type="text" placeholder="Buscar Finanças...">
             </div>
             
-            <span class="name-profile">Hi, <!--<< ?php echo $_SESSION['nome'];?> --></span>
+            <span class="name-profile">Hi, <?php echo $_SESSION['nome'];?></span>
             <i class="uil uil-user-circle"></i>
         </div>
 
@@ -120,9 +166,9 @@
                     <span class="text">Gestões recentes</span>
                 </div>
 
-                                    <!-- Botão para acionar modal -->
+            <!-- Botão para acionar modal -->
             <button type="button" class="btn btn-primary open" data-toggle="modal" data-target="#modalExemplo">
-              Adicionar nova gestão
+              Adicionar registro
             </button>
 
             <!-- Modal -->
@@ -137,20 +183,69 @@
                   </div>
                   <div class="modal-body">
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control form-control-sm lanca" id="floatingInput" placeholder="name@example.com" name="lanca">
-                            <label style="color: #000" for="lanca">Descrição da compra</label>
+                            <input type="text" class="form-control form-control-sm editora" id="descricao" name="ds_lancamento">
+                            <label style="color: #000" for="ds_lancamento">Descrição</label>
                 
                         </div>
-
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control form-control-sm senha" id="floatingInput" placeholder="name@example.com" name="senha">
-                            <label style="color: #000" for="senha">Senha</label>
+                            <input type="date" class="form-control form-control-sm editora" id="data" name="dt_lancamento">
+                            <label style="color: #000" for="dt_lancamento">Data</label>
                 
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="number" class="form-control form-control-sm editora" id="valor" name="vl_lancamento">
+                            <label style="color: #000" for="vl_lancamento">Valor</label>
+                
+                        </div>
+                        <div class="mb-3">
+                            <select class="form-select" id="categoria">
+                                <option value="" date-default disable selected>categoria</option>
+                                <?php 
+                                    require('php/conexao.php');
+                                    $option = "";
+                                    $sql = 'SELECT * FROM tb_categoria';
+
+                                    foreach ($conn->query($sql) as $row){
+                                        $option .= "<option value='".$row['cd_categoria']."'>".$row['nm_categoria']."</option>";
+                                    }
+                                    echo $option;
+                                ?>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <select class="form-select" id="pagamento">
+                                <option>Pagamento</option>
+                                <?php 
+                                    require('php/conexao.php');
+                                    $option = "";
+                                    $sql = "SELECT * FROM tb_forma_pagto";
+
+                                    foreach ($conn->query($sql) as $row){
+                                        $option .= "<option value='".$row['cd_forma_pagto']."'>".$row['nm_forma_pagto']."</option>";
+                                    }
+                                    echo $option;
+                                ?>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <select class="form-select" id="responsavel">
+                                <option>Responsável</option>
+                                <?php 
+                                    require('php/conexao.php');
+                                    $option = "";
+                                    $sql = 'SELECT * FROM tb_responsavel';
+
+                                    foreach ($conn->query($sql) as $row){
+                                        $option .= "<option value='".$row['cd_responsavel']."'>".$row['nm_responsavel']."</option>";
+                                    }
+                                    echo $option;
+                                ?>
+                            </select>
                         </div>
                   </div>
                   <div class="modal-footer">
-                      <button type="button" class="btn btn-danger">Concluir</button>
-                      <button type="button" class="btn btn-dark" data-dismiss="modal">Fechar</button>
+                      <button type="button" id="concluir" class="btn btn-danger">Concluir</button>
+                      <button type="button" id="fechar" class="btn btn-dark" data-dismiss="modal">Fechar</button>
                   </div>
                 </div>
               </div>
@@ -159,48 +254,23 @@
 <!-- Fim do modal -->
                 </div>
 
-                <div class="activity-data" style="display: flex; align-items:center; justify-content:center; ">
-                <table class="table" style="width:70vw;" >
-                    <thead class="thead-dark bg-dark text-white">
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <select class="form-select" aria-label="Default select example" style="margin-bottom: 1rem;">
-                            <option selected value="" data-default disabled selected>Selecione uma Categoria</option>
-                            <?php 
-                                 include('./php/conexao.php');
-                                 $option = "";
-                                 $sql = 'SELECT * FROM tb_categoria';
-                                foreach ($conn->query($sql) as $row){
-                                    $option .= "<option value='".$row['cd_categoria']."'>".$row['nm_categoria']."</option>";
-                                }
-                                 echo $option;
-                                ?>
-                        </select>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                    </table>
+                <div class="activity-data">
+					<table class="table" style="">
+							<thead class="thead-dark bg-dark text-white">
+								<tr>
+									<th scope="col">Descrição</th>
+									<th scope="col">Valor</th>
+									<th scope="col">Data</th>
+									<th scope="col">Categoria</th>
+									<th scope="col">Pagamento</th>
+									<th scope="col">Responsavel</th>
+									<th scope="col">Ações</th>
+								</tr>
+							</thead>
+							<tbody id="exibe">
+                                <!-- lancamentos -->
+							</tbody>
+						</table>
                 </div>
             </div>
         </div>
@@ -209,7 +279,7 @@
     <script src="js/toggle.js"></script>
     <!-- BootStrap SRC -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.6.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>>
 </body>
