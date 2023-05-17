f15ed8b888137341a965f75406d7ab5837fbd0aa
Amália Coelho <gray.coelho@gmail.com>
2023-05-16 21:11:28 -0300

    script cadastro
    correção do erro de não agregar o valor de $_SESSION[''] por falta de consulta

--- php/script_cadastro.php
+++ php/script_cadastro.php
@@ -7,10 +7,10 @@
         $stmt->bindValue(':email', $_POST['email']);
         $stmt->execute();
         
-        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+        $consulta = $stmt->fetch(PDO::FETCH_ASSOC);
         
         // SE O EMAIL JA FOR CADASTRADO
-        if ($usuario){
+        if ($consulta){
             echo 'Email já cadastrado, tente fazer login!';
         }else{
             $stmt = $conn->prepare("INSERT INTO tb_usuario (nm_usuario, sn_usuario, ds_login, ds_senha, nr_idade, id_nivel) VALUES(:nome, :sobrenome, :email, :senha, :idade, :nivel)");
@@ -23,6 +23,12 @@
                 ':nivel' => 1
             ));
             
+            $stmt_usuario = $conn->prepare("SELECT * FROM tb_usuario WHERE ds_login = :email");
+            $stmt_usuario->bindValue(':email', $_POST['email']);
+            $stmt_usuario->execute();
+            
+            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
             $_SESSION['email'] = $_POST['email'];
             $_SESSION['nome'] = $_POST['nome'];
             $_SESSION['cd'] = $usuario['cd_usuario'];
