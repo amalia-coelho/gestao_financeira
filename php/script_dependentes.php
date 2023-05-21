61fbd2a2cf2bc362c7f5b2bd8479289b50f905a6
Amália Coelho <gray.coelho@gmail.com>
2023-05-20 22:36:59 -0300

    Dependentes de dependentes
    Agora, quando um usuário for cadastrado como dependente, os dependentes dele virarão responsáveis

--- php/script_dependentes.php
+++ php/script_dependentes.php
@@ -10,15 +10,26 @@
         $dependente = $stmt->fetch(PDO::FETCH_ASSOC);
         
         // SE O EMAIL JA FOR CADASTRADO
-            # code...
             if ($dependente){
-                $stmt = $conn->prepare("UPDATE tb_usuario SET id_responsavel = :id_responsavel, id_nivel = 2 WHERE cd_usuario = :cd_usuario");
-                $stmt->execute(array(
-                    ':id_responsavel' => $_SESSION['cd'],
-                    ':cd_usuario' => $dependente['cd_usuario']
-                ));
-                
-                echo "Usuário cadastrado como dependente!";
+                if($dependente['id_nivel'] == 2){
+                    echo "O usuário já é dependente";
+                }else{
+                    //Cadastra o usuário como dependente
+                    $stmt = $conn->prepare("UPDATE tb_usuario SET id_responsavel = :id_responsavel, id_nivel = 2 WHERE cd_usuario = :cd_usuario");
+                    $stmt->execute(array(
+                        ':id_responsavel' => $_SESSION['cd'],
+                        ':cd_usuario' => $dependente['cd_usuario']
+                    ));
+                    
+                    echo "Usuário cadastrado como dependente!";
+
+                    //Define como responsável todos os dependentes deste usuário
+                    $sql = "SELECT * FROM tb_usuario WHERE id_responsavel =".$dependente['cd_usuario'];
+                    foreach ($conn->query($sql) as $descadastro) {
+                        $stmt = $conn->prepare("UPDATE tb_usuario SET id_responsavel = null, id_nivel = 1");
+                        $stmt->execute();
+                    }
+                }
             }else{
                 echo "Usuário não cadastrado ou email incorreto!";
             }
